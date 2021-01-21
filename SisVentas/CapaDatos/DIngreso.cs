4d1cf5b8485345c99c74468bf26cc432204a4cf1
Farid Josua Olmedo Lopez <68607809+Faridlpz@users.noreply.github.com>
2021-01-20 19:37:22 -0600

    Add files via upload

---
+++ SisVentas/CapaDatos/DIngreso.cs
@@ -0,0 +1,281 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class DIngreso
+    {
+        //Variables
+        private int _Idingreso;
+        private int _Idtrabajador;
+        private string _Tipo_Comprobante;
+        private string _Estado;
+        
+        //Propiedades
+        public int Idingreso
+        {
+            get { return _Idingreso; }
+            set { _Idingreso = value; }
+        }
+        
+        public int Idtrabajador
+        {
+            get { return _Idtrabajador; }
+            set { _Idtrabajador = value; }
+        }
+
+        public string Tipo_Comprobante
+        {
+            get { return _Tipo_Comprobante; }
+            set { _Tipo_Comprobante = value; }
+        }
+        public string Estado
+        {
+            get { return _Estado; }
+            set { _Estado = value; }
+        }
+        //Constructores
+        public DIngreso()
+        {
+
+        }
+
+        public DIngreso(int idingreso,int idtrabajador,int idproveedor,string tipo_comprobante, string estado)
+        {
+            this.Idingreso = idingreso;
+            this.Idtrabajador = idtrabajador;
+            this.Tipo_Comprobante = tipo_comprobante;
+            this.Estado = estado;
+        }
+        //Métodos
+        public string Insertar(DIngreso Ingreso,List<DDetalle_Ingreso> Detalle)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Establecer la trasacción
+                SqlTransaction SqlTra = SqlCon.BeginTransaction();
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.Transaction = SqlTra;
+                SqlCmd.CommandText = "spinsertar_ingreso";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdingreso = new SqlParameter();
+                ParIdingreso.ParameterName = "@idingreso";
+                ParIdingreso.SqlDbType = SqlDbType.Int;
+                ParIdingreso.Direction = ParameterDirection.Output;
+                SqlCmd.Parameters.Add(ParIdingreso);
+
+                SqlParameter ParIdtrabajador = new SqlParameter();
+                ParIdtrabajador.ParameterName = "@idtrabajador";
+                ParIdtrabajador.SqlDbType = SqlDbType.Int;
+                ParIdtrabajador.Value = Ingreso.Idtrabajador;
+                SqlCmd.Parameters.Add(ParIdtrabajador);
+
+                SqlParameter ParTipo_Comprobante = new SqlParameter();
+                ParTipo_Comprobante.ParameterName = "@tipo_comprobante";
+                ParTipo_Comprobante.SqlDbType = SqlDbType.VarChar;
+                ParTipo_Comprobante.Size = 20;
+                ParTipo_Comprobante.Value = Ingreso.Tipo_Comprobante;
+                SqlCmd.Parameters.Add(ParTipo_Comprobante);
+
+                SqlParameter ParEstado = new SqlParameter();
+                ParEstado.ParameterName = "@estado";
+                ParEstado.SqlDbType = SqlDbType.VarChar;
+                ParEstado.Size = 7;
+                ParEstado.Value = Ingreso.Estado;
+                SqlCmd.Parameters.Add(ParEstado);
+
+                //Ejecutamos nuestro comando
+
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "NO se Ingreso el Registro";
+
+                if (rpta.Equals("OK"))
+                {
+                    //Obtener el código del ingreso generado
+                    this.Idingreso = Convert.ToInt32(SqlCmd.Parameters["@idingreso"].Value);
+                    foreach (DDetalle_Ingreso det in Detalle)
+                    {
+                        det.Idingreso = this.Idingreso;
+                        //Llamar al método insertar de la clase DDetalle_Ingreso
+                        rpta = det.Insertar(det,ref SqlCon,ref SqlTra);
+                        if (!rpta.Equals("OK"))
+                        {
+                            break;
+                        }
+                    }
+
+                }
+                if (rpta.Equals("OK"))
+                {
+                    SqlTra.Commit();
+                }
+                else
+                {
+                    SqlTra.Rollback();
+                }
+                
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+
+        }
+        public string Anular(DIngreso Ingreso)
+        {
+            string rpta = "";
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                //Código
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCon.Open();
+                //Establecer el Comando
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spanular_ingreso";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParIdingreso = new SqlParameter();
+                ParIdingreso.ParameterName = "@idingreso";
+                ParIdingreso.SqlDbType = SqlDbType.Int;
+                ParIdingreso.Value = Ingreso.Idingreso;
+                SqlCmd.Parameters.Add(ParIdingreso);
+
+
+                //Ejecutamos nuestro comando
+
+                rpta = SqlCmd.ExecuteNonQuery() == 1 ? "OK" : "NO se anulo el Registro";
+
+
+            }
+            catch (Exception ex)
+            {
+                rpta = ex.Message;
+            }
+            finally
+            {
+                if (SqlCon.State == ConnectionState.Open) SqlCon.Close();
+            }
+            return rpta;
+        }
+
+        //Método Mostrar
+        public DataTable Mostrar()
+        {
+            DataTable DtResultado = new DataTable("ingreso");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostrar_ingreso";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+
+        }
+
+
+        //Método Buscarfechas
+        public DataTable BuscarFechas(String TextoBuscar,String TextoBuscar2)
+        {
+            DataTable DtResultado = new DataTable("ingreso");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spbuscar_ingreso_fecha";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlParameter ParTextoBuscar2 = new SqlParameter();
+                ParTextoBuscar2.ParameterName = "@textobuscar2";
+                ParTextoBuscar2.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar2.Size = 50;
+                ParTextoBuscar2.Value = TextoBuscar2;
+                SqlCmd.Parameters.Add(ParTextoBuscar2);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+
+        }
+
+
+        public DataTable MostrarDetalle(String TextoBuscar)
+        {
+            DataTable DtResultado = new DataTable("detalle_ingreso");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "spmostrar_detalle_ingreso";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter ParTextoBuscar = new SqlParameter();
+                ParTextoBuscar.ParameterName = "@textobuscar";
+                ParTextoBuscar.SqlDbType = SqlDbType.VarChar;
+                ParTextoBuscar.Size = 50;
+                ParTextoBuscar.Value = TextoBuscar;
+                SqlCmd.Parameters.Add(ParTextoBuscar);
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+
+        }
+
+
+    }
+}
---
+++ SisVentas/CapaDatos/Dfaltantes.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class Dfaltantes
+    {
+        //Mostrar Artículos Faltantes
+        public DataTable Mostrar_Faltantes()
+        {
+            DataTable DtResultado = new DataTable("articulos");
+            SqlConnection SqlCon = new SqlConnection();
+            try
+            {
+                SqlCon.ConnectionString = Conexion.Cn;
+                SqlCommand SqlCmd = new SqlCommand();
+                SqlCmd.Connection = SqlCon;
+                SqlCmd.CommandText = "sp_mostrar_faltantes";
+                SqlCmd.CommandType = CommandType.StoredProcedure;
+
+                SqlDataAdapter SqlDat = new SqlDataAdapter(SqlCmd);
+
+                SqlDat.Fill(DtResultado);
+
+            }
+            catch (Exception ex)
+            {
+                DtResultado = null;
+            }
+            return DtResultado;
+
+        }
+    }
+}
